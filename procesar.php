d0dd60ec42393e9d9fa37a651757bfe69a866357
D3C0DE <juanpabloruizcruz4@gmail.com>
2026-04-12 09:11:23 -0500

    Subida del proyecto

---
+++ procesar.php
@@ -0,0 +1,23 @@
+<?php
+$conexion = new mysqli("localhost", "root", "", "lasalle");
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
+
+$sql = "INSERT INTO contactos (nombre, correo, asunto, mensaje)
+        VALUES ('$nombre', '$correo', '$asunto', '$mensaje')";
+
+if ($conexion->query($sql) === TRUE) {
+    echo "Mensaje enviado correctamente";
+} else {
+    echo "Error: " . $conexion->error;
+}
+
+$conexion->close();
+?>
